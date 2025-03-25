d41a4a3b9f4a3c2bead20b349bdf7b6d230ec6ce
Martin Salomon <salomonmartin21@gmail.com>
2025-03-25 11:30:05 -0300

    cambio en el toggle-menu

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const nav = document.querySelector('nav');
 
 menuToggle.addEventListener('click', () => {
     nav.classList.add('active');
+    menuToggle.style = "display: none;"
 });
 
 closeBtn.addEventListener('click', () => {
     nav.classList.remove('active');
+    menuToggle.style = "display: block;"
 });
